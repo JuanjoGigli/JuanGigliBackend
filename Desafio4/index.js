82cb1fb9e048222f21c9c6df567e793c203778bd
JuanjoGigli <cachirli20@gmail.com>
2022-12-15 18:49:11 -0300

    apirest

--- Desafio4/index.js
+++ Desafio4/index.js
@@ -1,40 +1,88 @@
-const express = require('express');
-const { Router } = express;
+// >> Consigna:
+// Realizar un proyecto de servidor basado en node.js y express que ofrezca una API RESTful de productos.
+// En detalle, que incorpore las siguientes rutas:
+// GET '/api/productos' -> devuelve todos los productos.
+// GET '/api/productos/:id' -> devuelve un producto según su id.
+// POST '/api/productos' -> recibe y agrega un producto, y lo devuelve con su id asignado.
+// PUT '/api/productos/:id' -> recibe y actualiza un producto según su id.
+// DELETE '/api/productos/:id' -> elimina un producto según su id.
 
-const app = express();   
 
-app.use(express.json()); 
+// # Para el caso de que un producto no exista, se devolverá el objeto: { error : 'producto no encontrado' }
+// # Implementar la API en una clase separada, utilizando un array como soporte de persistencia en memoria.
+// # Incorporar el Router de express en la url base '/api/productos' y configurar las subrutas en base a este.
+// # Crear un espacio público de servidor que contenga un index.html con un formulario de ingreso de productos.
+// # El servidor debe estar basado en express y debe implementar los mensajes de conexión al puerto 8080.
+// # En caso de error, representar la descripción del mismo.
+// # Las respuestas del servidor serán en formato JSON.
+// # La funcionalidad será probada a través de Postman y del formulario de ingreso.
+
+const { Router } = require("express");
+const express = require("express");
+const app = express();
+
+app.use(express.json());
 
 const productRouter = Router(); 
 
+const arrayProduct= [];
 
-app.use('/api/productos', productRouter);
-app.use('/api/productos/:id', productRouter);
+app.use("/api/productos", productRouter);
+app.use("/api/productos/:id", productRouter)
 
-productRouter.get('/', (req, res) => {
-    res.send(`GET All Products`);
+// GET request que retorna todo el array de productos en formato JSON.
+apiProductRouter.get('/', (req, res) => {
+    res.json(arrayProducts);
 });
 
-productRouter.get('/:id', (req, res) => {
+// GET request que recibe ID para retornar el objeto que coincida con el ID indicado.
+apiProductRouter.get('/:id', (req, res) => {
+    const { id } = req.params;                                          
+    let objectById = arrayProducts.find(element => element.id == id);   
+    if(objectById != undefined){                                        
+        res.json(objectById);                                           
+    } else {
+        res.send("NOT FOUND");                                          
+    }
+});
+
+// POST request que recibe en formato JSON las key de un nuevo producto a ser agregado.
+// En nuevo ID para el producto es generado en este metodo tembien.
+apiProductRouter.post('/', (req, res) => {
+    const { nombre,precio  } = req.body;                       
+    const newId = arrayProducts.length + 1;                 
+    const newObj = { id: newId, name: name, precio: precio }; 
+    arrayProducts.push(newObj);                             
+    res.json(newObj);                                       
+});
+
+// PUT request que actualiza un producto por su ID.
+apiProductRouter.put('/:id', (req, res) => {
     const { id } = req.params;
-    res.send(`GET Product Id: ${id}`);
+    const { name, precio } = req.body;
+
+    const indexObjet = arrayProducts.findIndex(element => element.id == id);
+    if ( indexObjet != -1 ) {
+        arrayProducts[indexObjet].name  = name;
+        arrayProducts[indexObjet].image = imgurl;
+        res.json(arrayProducts[indexObjet]);
+    } else {
+        res.send("NOT FOUND - NOT UPDATED"); 
+    }
 });
 
-productRouter.post('/', (req, res) => {
-    res.send("Request POST Add");
-});
-
-productRouter.put('/:id', (req, res) => {
+// DELETE request que borra el producto del ID indicado si existe.
+apiProductRouter.delete('/:id', (req, res) => {
     const { id } = req.params;
-    res.send(`Request PUT Update Id: ${id}`);
+    const indexObjet = arrayProducts.findIndex(element => element.id == id);    
+    if ( indexObjet != -1 ) {                                                   
+        const deletedProd = arrayProducts.splice(indexObjet, 1);                
+        res.json(deletedProd);                                                  
+    } else {
+        res.send("NOT FOUND - NOT DELETED");                                    
+    }
 });
-
-productRouter.delete('/:id', (req, res) => {
-    const { id } = req.params;
-    res.send(`Request DELETE Id: ${id}`);
-});
-
-
 
 const PORT = 8080;
 app.listen(PORT, ()=> console.log(`Listening on port: ${PORT}`));
+
